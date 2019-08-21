4d39cd4c76ff9a04407c5b78f1ffbaaf15bf9030
ek-nz <ek@ek.net.nz>
2019-08-21 14:44:43 +1200

    Tidy up and comment

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 (function(){
+'use strict'; //From LinkedIn Learning video "boilerplate"
 
-    'use strict';
+// Wait for the page to load
 document.addEventListener('DOMContentLoaded', function(){
     
+    // Store the element to display the time into
     var theTimeNow = document.getElementById('the-time-now');
     
+    //Make the updateTime function run every 1 second
     setInterval(updateTime, 1000);
+    
+    //Update the time in the browser (runs every second)
     function updateTime() {
         var now = new Date();
         theTimeNow.innerHTML = now;
