3153df5a2f540ca7b84ca3e3ce693c1e54179bc1
ek-nz <ek@ek.net.nz>
2019-08-21 12:03:31 +1200

    Add time placeholder

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 (function(){
+
     'use strict';
+document.addEventListener('DOMContentLoaded', function(){
     
-    
+    var theTimeNow = document.getElementById('the-time-now');
+
+    theTimeNow.innerHTML = "the time now";
+});
 })();
